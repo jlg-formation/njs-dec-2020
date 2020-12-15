e9c134f686317c9149e18793f9f441c256fc0e94
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-15 12:23:07 +0100

    hello

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'front';
+  name = 'Jean-Louis';
+
+  constructor() {
+    interval(1000).subscribe((x) => {
+      if (this.name === 'Jean-Louis') {
+        this.name = 'Jean-Marc';
+      } else {
+        this.name = 'Jean-Louis';
+      }
+    });
+  }
 }
