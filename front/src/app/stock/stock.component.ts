dd7fb30d9db9c4368c1fc5241afb3bb6f124e3e4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-16 11:50:45 +0100

    added service

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../interfaces/article';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
+  articles: Article[] = [
+    { name: 'Tournevis', price: 2.34, qty: 120 },
+    { name: 'Tournevis Cruciforme', price: 2.55, qty: 55 },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
