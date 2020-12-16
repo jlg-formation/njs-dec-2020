b315fe3ed06887f5145c1e51e99cd5e6c3753b4b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-16 12:24:12 +0100

    form ok

--- front/src/app/stock/stock.module.ts
+++ front/src/app/stock/stock.module.ts
@@ -4,13 +4,10 @@ import { CommonModule } from '@angular/common';
 import { StockRoutingModule } from './stock-routing.module';
 import { StockComponent } from './stock.component';
 import { CreateComponent } from './create/create.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [StockComponent, CreateComponent],
-  imports: [
-    CommonModule,
-    StockRoutingModule
-  ]
+  imports: [CommonModule, StockRoutingModule, ReactiveFormsModule],
 })
-export class StockModule { }
+export class StockModule {}
