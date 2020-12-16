d6eb7d822e968a5ccab6712f197459e6fab2c1d8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-16 16:19:33 +0100

    localstorage

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -1,18 +1,32 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { Article } from '../interfaces/article';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ArticleService {
-  articles: Article[] = [
-    { name: 'Tournevis', price: 2.34, qty: 120 },
-    { name: 'Tournevis Cruciforme', price: 2.55, qty: 55 },
-  ];
+  articles$ = new BehaviorSubject<Article[]>(this.getArticles());
 
-  constructor() {}
+  constructor() {
+    this.articles$.subscribe((articles) =>
+      localStorage.setItem('articles', JSON.stringify(articles))
+    );
+  }
+
+  getArticles(): Article[] {
+    const str = localStorage.getItem('articles');
+    if (!str) {
+      return [
+        { name: 'Tournevis', price: 2.34, qty: 120 },
+        { name: 'Tournevis Cruciforme', price: 2.55, qty: 55 },
+      ];
+    }
+    return JSON.parse(str) as Article[];
+  }
 
   add(article: Article): void {
-    this.articles.push(article);
+    this.articles$.value.push(article);
+    this.articles$.next(this.articles$.value);
   }
 }
