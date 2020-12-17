08eb9e4f72082635dd103b2138a98090fe7fa378
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-17 14:35:03 +0100

    insert in postgre ok

--- back/src/test-db.ts
+++ back/src/test-db.ts
@@ -4,10 +4,18 @@ import {db1} from '../database.json';
 async function main() {
   const client = new Client(db1);
   await client.connect();
-  const res = await client.query('SELECT $1::text as message', [
-    'Hello world!',
-  ]);
-  console.log(res.rows[0].message); // Hello world!
+
+  const text =
+    'INSERT INTO articles(name, price, qty) VALUES($1, $2, $3) RETURNING *';
+  const values = ['Tournevis', 2.33, 123];
+
+  try {
+    const res = await client.query(text, values);
+    console.log(res.rows[0]);
+  } catch (err) {
+    console.log(err.stack);
+  }
+
   await client.end();
 }
 
