74541875421faab9be4040680d742641ebe1ee1b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-12-17 11:21:18 +0100

    added observable on back

--- back/src/ws.ts
+++ back/src/ws.ts
@@ -1,13 +1,23 @@
 import express from 'express';
-import {readFileSync} from 'fs';
+// eslint-disable-next-line node/no-unsupported-features/node-builtins
+import {readFileSync, promises} from 'fs';
 import {resolve} from 'path';
 import {Article} from './Article';
+import {BehaviorSubject} from 'rxjs';
 
 const filename = resolve(__dirname, '../data/articles.json');
 
 let articles = JSON.parse(
   readFileSync(filename, {encoding: 'utf8'})
 ) as Article[];
+
+const articles$ = new BehaviorSubject<Article[]>(articles);
+
+articles$.subscribe(myArticles => {
+  (async () => {
+    await promises.writeFile(filename, JSON.stringify(myArticles, null, 2));
+  })();
+});
 
 let nextId = 3;
 
@@ -19,17 +29,19 @@ app.get('/articles', (req, res) => {
   res.json(articles);
 });
 
-app.post('/articles', (req, res) => {
+app.post('/articles', async (req, res) => {
   const article = req.body as Article;
   article.id = 'a' + nextId;
   nextId++;
   articles.push(article);
+  articles$.next(articles);
   res.status(201).json(article);
 });
 
-app.delete('/articles', (req, res) => {
+app.delete('/articles', async (req, res) => {
   const ids = req.body as string[];
   articles = articles.filter(a => !ids.includes(a.id));
+  articles$.next(articles);
   res.status(204).end();
 });
 
